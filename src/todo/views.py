0a4e22cce40add71abe8774060634c05a922bb83
jungminchae <minchae3618@gmail.com>
2024-08-09 17:21:32 +0900

    feat: add task list view

--- src/todo/views.py
+++ src/todo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from todo.serializers import TaskSerializer
+from todo.models import Task
 
-# Create your views here.
+
+class TaskListView(ListAPIView):
+    model = Task
+    serializer_class = TaskSerializer
