f14301ece2bb3dffaec40b98136b1d9f944d6484
jungminchae <minchae3618@gmail.com>
2024-08-09 23:42:24 +0900

    refactor: generic views to viewsets

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from todo.views import TaskListView, ListCreateView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from todo.views import TaskViewSet, ListCreateView
+
+
+router = DefaultRouter()
+
+router.register("tasks", TaskViewSet)
 
 urlpatterns = [
-    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("", include(router.urls), name="tasks"),
     path("lists/", ListCreateView.as_view(), name="list-create"),
 ]
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,5 +1,6 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
-from todo.serializers import TaskSerializer, ListSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.viewsets import ModelViewSet
+from todo.serializers import TaskSerializer, ListSerializer, TaskCreateSerializer
 from todo.models import Task, List
 
 
@@ -8,7 +9,12 @@ class ListCreateView(CreateAPIView):
     serializer_class = ListSerializer
 
 
-class TaskListView(ListAPIView):
+class TaskViewSet(ModelViewSet):
     model = Task
     serializer_class = TaskSerializer
     queryset = Task.objects.all().order_by("-created_date")
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return TaskCreateSerializer
+        return TaskSerializer
